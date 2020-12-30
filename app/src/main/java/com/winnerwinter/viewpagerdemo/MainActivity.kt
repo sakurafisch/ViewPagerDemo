218554928c1b3dd99b6c89210e09f7beae72de5e
sakurafisch <winnerwinter1998@gmail.com>
2020-12-30 17:21:02 +0800

    TabLayout 和 ViewPager2 双向绑定

--- app/src/main/java/com/winnerwinter/viewpagerdemo/MainActivity.kt
+++ app/src/main/java/com/winnerwinter/viewpagerdemo/MainActivity.kt
@@ -15,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.viewPager2.adapter = object: FragmentStateAdapter(this) {
+            override fun getItemCount(): Int = 3
+
+            override fun createFragment(position: Int) =
+                when(position) {
+                    0 -> ScaleFragment()
+                    1 -> RotateFragment()
+                    else -> TranslateFragment()
+            }
+        }
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) {tab, position ->
+            when (position) {
+                0 -> tab.text = "缩放"
+                1 -> tab.text = "旋转"
+                2 -> tab.text = "移动"
+            }
+        }.attach()
     }
 }
