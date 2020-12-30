1369ffc0b3c40cbab010a658c8fb2fe697f3df4b
sakurafisch <winnerwinter1998@gmail.com>
2020-12-30 17:18:57 +0800

    Initial commit

---
+++ app/src/main/java/com/winnerwinter/viewpagerdemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.winnerwinter.viewpagerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.winnerwinter.viewpagerdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
